199b77072b371a7e8fe1e6ac7b5f25bbb8b3a3ba
Aditya <akarnam37@gmail.com>
2026-08-25 01:50:32 -0500

    feat(provider-sdk): implement ContextStore SPI contracts and error definitions

---
+++ packages/provider-sdk/src/errors.ts
@@ -0,0 +1,20 @@
+export class DriverNotInstalledError extends Error {
+  constructor(public readonly scheme: string, public readonly packageName: string, public readonly reason?: unknown) {
+    super(`${scheme} driver is not installed.\nInstall it with:  npm install ${packageName}`);
+    this.name = 'DriverNotInstalledError';
+  }
+}
+
+export class InvalidDsnError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = 'InvalidDsnError';
+  }
+}
+
+export class ConcurrencyConflictError extends Error {
+  constructor(public readonly contextId: string, public readonly expectedRevision: number, public readonly actualRevision: number) {
+    super(`Concurrency conflict on context '${contextId}': expected revision ${expectedRevision}, but found ${actualRevision}`);
+    this.name = 'ConcurrencyConflictError';
+  }
+}
---
+++ packages/provider-sdk/src/index.ts
@@ -0,0 +1,2 @@
+export * from './spi.js';
+export * from './errors.js';
